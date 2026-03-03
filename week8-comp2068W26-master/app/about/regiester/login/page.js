2453b152770ed298f45d49bc9f059ce00e9832df
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2026-03-03 15:19:16 -0500

    Add files via upload

---
+++ week8-comp2068W26-master/app/about/regiester/login/page.js
@@ -0,0 +1,21 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login to Your Account</h1>
+
+      <form>
+        <div>
+          <label>Email:</label>
+          <input type="email" name="email" required />
+        </div>
+
+        <div>
+          <label>Password:</label>
+          <input type="password" name="password" required />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
---
+++ week8-comp2068W26-master/app/sushi/page.tsx
@@ -0,0 +1,7 @@
+export default function Sushi() {
+  return (
+    <main>
+      <h1>Our Sushi Menu</h1>
+    </main>
+  );
+}
